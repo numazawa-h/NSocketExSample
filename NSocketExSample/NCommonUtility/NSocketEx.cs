a38b2ec3dd7ec1dbaa8cdca1f8af64bd3c1c8786
numazawa-h <numazawa.h@gmail.com>
2025-01-11 07:25:18 +0900

    データ長にヘッダサイズを含めるか否かの指定を追加

--- NSocketExSample/NCommonUtility/NSocketEx.cs
+++ NSocketExSample/NCommonUtility/NSocketEx.cs
@@ -29,6 +29,7 @@ namespace NCommonUtility
         public int HeaderSize { get; private set; }
         public int DataLenOffset { get; private set; }
         public int DataLenSize { get; private set; }
+        public bool DataLenIsPacketSize { get; private set; }
 
         public event SendRecvExEventHandler OnSendExEvent;
         public event SendRecvExEventHandler OnRecvExEvent;
@@ -40,17 +41,18 @@ namespace NCommonUtility
         private int _data_recv_cnt = -1;
         private int _data_length = -1;
 
-        public NSocketEx(int hsize, int dlen_ofs, int dlen_size=2)
+
+        public NSocketEx(int hsize, int dlen_ofs, int dlen_size=2, bool bPacket=false)
         {
-            init(hsize, dlen_ofs, dlen_size);
+            init(hsize, dlen_ofs, dlen_size, bPacket);
         }
 
-        public NSocketEx(Socket soc, int hsize, int dlen_ofs, int dlen_size = 2) : base(soc)
+        public NSocketEx(Socket soc, int hsize, int dlen_ofs, int dlen_size = 2, bool bPacket = false) : base(soc)
         {
-            init(hsize, dlen_ofs, dlen_size);
+            init(hsize, dlen_ofs, dlen_size, bPacket);
         }
 
-        private void init(int hsize, int dlen_ofs, int dlen_size)
+        protected void init(int hsize, int dlen_ofs, int dlen_size, bool bPacket)
         {
             if (dlen_ofs < 0)
             {
@@ -64,6 +66,7 @@ namespace NCommonUtility
             {
                 throw new ArgumentException("ヘッダサイズが小さすぎます");
             }
+            DataLenIsPacketSize = bPacket;
             HeaderSize = hsize;
             DataLenOffset = dlen_ofs;
             DataLenSize = dlen_size;
@@ -161,7 +164,13 @@ namespace NCommonUtility
                 Array.Reverse(dlen);
             }
 
-            return BitConverter.ToInt32(dlen, 0);
+            int len = BitConverter.ToInt32(dlen, 0);
+            if (DataLenIsPacketSize)
+            {
+                // ヘッダのレングスがヘッダ長を含むならヘッダ長を引く
+                len -= HeaderSize;
+            }
+            return len;
         }
 
         protected virtual void OnSend(byte[] hed, byte[] dat)
@@ -201,7 +210,7 @@ namespace NCommonUtility
             try
             {
                 Socket soc = socket._soc.EndAccept(ar);
-                OnAccept(new NSocketEx(soc, this.HeaderSize, this.DataLenOffset, this.DataLenSize));
+                OnAccept(new NSocketEx(soc, this.HeaderSize, this.DataLenOffset, this.DataLenSize, this.DataLenIsPacketSize));
             }
             catch (Exception ex)
             {
